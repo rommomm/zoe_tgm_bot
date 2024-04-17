fc0e08560f7e73b96cd6f76e9dac29fb69711b8f
rommomm <rommomm@i.ua>
2024-04-17 22:04:05 +0300

    1

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,9 +1,13 @@
-import TeleBot from "telebot"
-import shortReply from "telebot/plugins/shortReply.js"
+import TeleBot from 'telebot'
+import shortReply from 'telebot/plugins/shortReply.js'
 
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
-bot.on("text", msg => msg.reply.text(msg.text))
+bot.on('text', msg => msg.reply.text(msg.text))
+
+bot.on('/start', msg => {
+	return bot.sendMessage(msg.from.id, 'Hello')
+})
 
 bot.plug(shortReply)
 
